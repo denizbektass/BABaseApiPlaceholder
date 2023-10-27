da62fe91d9ac392d0aecda48f0686acd1d482c9b
Emin Arıkan <muhammeteminarikan@gmail.com>
2023-10-27 14:20:41 +0300

    init

---
+++ src/main/java/com/bilgeadam/BABaseApiPlaceholder/BaBaseApiPlaceholderApplication.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.BABaseApiPlaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaBaseApiPlaceholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaBaseApiPlaceholderApplication.class, args);
+	}
+
+}
